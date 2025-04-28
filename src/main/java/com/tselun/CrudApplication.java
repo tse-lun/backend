923dd39599043da0b94d726771febcd3f1f5406c
tse-lun <tse_lun@hotmail.com>
2025-04-29 02:26:18 +0800

    init

--- src/main/java/com/tselun/CrudApplication.java
+++ src/main/java/com/tselun/CrudApplication.java
@@ -2,12 +2,29 @@ package com.tselun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("http://localhost:/*");
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
     }
+
+
 
 }
