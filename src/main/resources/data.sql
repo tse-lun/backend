aad43724f5c8f50fa441dbfb919f6930f796ade0
tse-lun <tse_lun@hotmail.com>
2025-04-28 23:58:34 +0800

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@example.com');
+INSERT INTO users (name, email) VALUES ('Charlie', 'charlie@example.com');
